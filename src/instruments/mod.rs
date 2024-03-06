2274c438a7f5e7fc762cd939534041affc4cc100
Mateo Bourji <mrbourji@gmail.com>
2024-03-06 21:21:57 +0000

    Missing update to Value trait signature

--- src/instruments/mod.rs
+++ src/instruments/mod.rs
@@ -12,7 +12,7 @@ pub enum OptionType {
 }
 
 pub trait Value {
-    fn calculate_payoff(&self, price_path: Vec<f64>) -> CashFlow;
+    fn calculate_payoff(&self, price_path: &Vec<f64>) -> CashFlow;
 
     fn settlement_datetime(&self) -> DateTime<Utc>;
     
